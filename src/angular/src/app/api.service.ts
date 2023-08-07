18c88d81b64429234e5b5dea9e027f2c1782816a
Danilo <danilodjuric2003@gmail.com>
2023-08-07 17:51:21 +0200

    start?

---
+++ src/angular/src/app/api.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private baseUrl = 'http://localhost:8080';
+  constructor(private http: HttpClient) {}
+}
