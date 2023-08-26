2e3a14af81a74f1a7d0f43bd64ab04b5b371322e
Danilo <danilodjuric2003@gmail.com>
2023-08-26 13:47:49 +0200

    Functioning Dashboard + Bar Components

--- src/angular/src/app/app.module.ts
+++ src/angular/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { TopbarComponent } from './topbar/topbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    SidebarComponent,
+    TopbarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/angular/src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: [
+    './sidebar.component.css',
+    '../../assets/bootstrap/css/bootstrap.min.css',
+    '../../assets/css/Team-Horizontal-icons.css',
+    '../../assets/css/Team-Horizontal-images.css',
+    '../../assets/fonts/fontawesome-all.min.css'
+  ]
+})
+export class SidebarComponent {}
