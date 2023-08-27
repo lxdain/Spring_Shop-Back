e2dd24d3a1fe3a99ed33dd804885eda74a5df632
Danilo <danilodjuric2003@gmail.com>
2023-08-27 20:34:08 +0200

    Improved Customer + Products UI

--- src/angular/src/app/customers/customers.component.ts
+++ src/angular/src/app/customers/customers.component.ts
@@ -6,9 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: [
     './customers.component.css',
     '../../assets/bootstrap/css/bootstrap.min.css',
+    '../../assets/fonts/fontawesome-all.min.css',
+    '../../assets/fonts/font-awesome.min.css',
+    '../../assets/fonts/fontawesome5-overrides.min.css',
+    '../../assets/css/Animated-Pretty-Product-List-v12-Animated-Pretty-Product-List.css',
+    '../../assets/css/Contact-Directory.css',
+    '../../assets/css/Manage-Users.css',
     '../../assets/css/Team-Horizontal-icons.css',
-    '../../assets/css/Team-Horizontal-images.css',
-    '../../assets/fonts/fontawesome-all.min.css'
+    '../../assets/css/Team-Horizontal-images.css'
   ]
 })
 export class CustomersComponent {}
--- src/angular/src/app/products/products.component.ts
+++ src/angular/src/app/products/products.component.ts
@@ -8,7 +8,10 @@ import { Component } from '@angular/core';
     '../../assets/bootstrap/css/bootstrap.min.css',
     '../../assets/css/Team-Horizontal-icons.css',
     '../../assets/css/Team-Horizontal-images.css',
-    '../../assets/fonts/fontawesome-all.min.css'
+    '../../assets/fonts/fontawesome-all.min.css',
+    '../../assets/css/Animated-Pretty-Product-List-v12-Animated-Pretty-Product-List.css',
+    '../../assets/css/Contact-Directory.css',
+    '../../assets/css/Manage-Users.css'
   ]
 })
 export class ProductsComponent {}
